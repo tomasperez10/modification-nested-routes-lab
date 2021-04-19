9886cf54bd23dc413d0099106dfc7d08256523c4
Tomas Perez <tomasitopg6@gmail.com>
2021-04-19 23:53:36 +0000

    done

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :artists do
     resources :songs, only: [:index, :show, :new, :edit]
   end
-  resources :songs
+  resources :songs do
+    resources :artists, only: [:edit]
+  end
 end
